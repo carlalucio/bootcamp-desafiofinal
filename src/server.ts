fe1d3141898962b3fa10b428dba00ef5de4d0f09
carlalucio <103859003+carlalucio@users.noreply.github.com>
2022-06-02 16:01:34 -0300

    teste

--- src/server.ts
+++ src/server.ts
@@ -8,13 +8,11 @@ import { resolve } from 'path';
 const PORT = env.PORT || 3000;
 const app = express();
 app.use(express.json());
-//usar o arquivo de rotas
 app.use(routes);
 app.use(errorHandler);
 app.use('/files', express.static(resolve(__dirname, '..', 'uploads')))
 
 
-//Cria uma porta e inicializar o banco de dados
 AppDataSource.initialize()
     .then(() => {
         app.listen(PORT, () => console.log(`Server in running in port ${PORT}`));
