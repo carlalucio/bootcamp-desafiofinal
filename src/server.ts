cdc16faf00bdad92a5afb94381489fa9356052f0
carlalucio <103859003+carlalucio@users.noreply.github.com>
2022-06-02 19:11:21 -0300

    teste15

--- src/server.ts
+++ src/server.ts
@@ -3,13 +3,13 @@ import { routes } from './routes';
 import {env} from './config/environment-variables';
 import { AppDataSource } from './config/data-source';
 import { resolve } from 'path';
-import { errorHandler } from './middlewares/index';
+//import { errorHandler } from './middlewares/index';
 
 const PORT = env.PORT || 3000;
 const app = express();
 app.use(express.json());
 app.use(routes);
-app.use(errorHandler);
+//app.use(errorHandler);
 app.use('/files', express.static(resolve(__dirname, '..', 'uploads')))
 
 
